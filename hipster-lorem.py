baee3a2c5a40928f34e5051bc95c9427cd2a90e5
maximefyodorov <maxime.fyodorov@gmail.com>
2020-09-15 12:01:11 +0300

    Hipster-lorem script added

---
+++ hipster-lorem.py
@@ -0,0 +1,16 @@
+import urllib.parse
+import urllib.request
+import json
+
+url = 'https://hipsum.co/api/'
+values = {  'type': 'hipster-centric',
+            'sentences': 5
+}
+
+# data = urllib.parse.urlencode(values)
+# url = '?'.join([url, data])
+# response = urllib.request.urlopen(url)
+# json_obj = json.loads(response.read())
+# print(json_obj[0])
+
+print(json.loads(urllib.request.urlopen('?'.join([url, urllib.parse.urlencode(values)])).read())[0])
