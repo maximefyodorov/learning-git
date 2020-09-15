81724d6924803652b8f7f4502457e18b4c48bea7
maximefyodorov <maxime.fyodorov@gmail.com>
2020-09-15 12:18:08 +0300

    Some values added and changed

--- hipster-lorem.py
+++ hipster-lorem.py
@@ -3,7 +3,7 @@ import urllib.request
 import json
 
 url = 'https://hipsum.co/api/'
-values = { 'type': 'hipster-centric', 'sentences': 5 }
+values = { 'type': 'hipster-centric', 'sentences': 5, 'start-with-lorem': 1 }
 
 # data = urllib.parse.urlencode(values)
 # url = '?'.join([url, data])
